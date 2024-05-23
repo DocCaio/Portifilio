6b2e865cb7f5a80bc22f36e539f1a60f3927249a
Caio <108128119+DocCaio@users.noreply.github.com>
2024-05-23 14:50:46 -0300

    up

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,16 @@ import './Hero.css';
 const Hero = () => {
     return (
        <section className='hero-container'>
-        <div></div>
+        <div className='hero-content'>
+            <h2>escrever</h2>
+            <p>elogios</p>
+
+        </div>
+
+        <div className='hero-img'>
+           <img src='./assets/programador.jpg' alt='programming guy' />
+
+        </div>
 
        </section>
 
