d91d0e22a1481851fccf399998587e962cc8a0c4
Caio <108128119+DocCaio@users.noreply.github.com>
2024-05-28 14:58:45 -0300

    up

---
+++ src/components/ContactInfoCards/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles.css';
+
+const   ContactInfoCards = ({ iconUrl ,text}) => {
+  return (
+   <div className="contact-details-card">
+    <div className="icon"></div>
+ 
+   </div>
+  )
+}
+
+export default ContactInfoCards;
--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import './styles.css';
+import ContactInfoCards from "../ContactInfoCards";
 
 const   ContactMe = () => {
   return (
    <section className="contact-container">
     <h5>Contact Me</h5>
-   <div className="contact-content"></div>
+   <div className="contact-content">
+    <div style={{flex:1}}>
+      <ContactInfoCards
+       iconUrl=""
+       text="Caiomib@outlook.com"
+
+      />
+        <ContactInfoCards
+       iconUrl=""
+       text="https://www.linkedin.com/in/caio-martins-2ba009207/?original_referer=https%3A%2F%2Fgithub.com%2F"
+
+      />
+      
+      
+    </div>
+    <div style={{flex:1}}></div>
+   </div>
+
    </section>
   )
 
