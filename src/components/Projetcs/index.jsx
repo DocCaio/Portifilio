4050fefc7f47ab4954b017f21f2a28085e6f0a3d
Caio <108128119+DocCaio@users.noreply.github.com>
2024-05-30 07:38:37 -0300

    up

--- src/components/Projetcs/index.jsx
+++ src/components/Projetcs/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './styles.css';
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Nav, Row } from "react-bootstrap";
 
 
 export const Projets = () => {  
@@ -13,6 +13,18 @@ export const Projets = () => {
             <Col>
                 <h2>Projetct</h2>
                 <p>Description</p>
+                
+                <Nav variant="pills"  defaultActiveKey="/home">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Active</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link  eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
             </Col>
         </Row>
     </Container>
