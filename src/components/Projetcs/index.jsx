6c7ee794a801390997141e30ec2eb1b2cb51f64e
Caio <108128119+DocCaio@users.noreply.github.com>
2024-05-31 09:42:54 -0300

    up

--- src/components/Projetcs/index.jsx
+++ src/components/Projetcs/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './styles.css';
-import { Col, Container, Nav, Row } from "react-bootstrap";
-
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
+import Pro from '../../util/pro.js';
 
 export const Projets = () => {  
-    
+<Pro/>
+  
     return (
     
   <section className="project" id="projetc">
@@ -13,7 +14,7 @@ export const Projets = () => {
             <Col>
                 <h2>Projetct</h2>
                 <p>Description</p>
-                
+                <Tab.Container id="projetcts-tabs" defaultActiveKey="fist">
                 <Nav variant="pills"  defaultActiveKey="/home">
                     <Nav.Item>
                       <Nav.Link eventKey="first">Active</Nav.Link>
@@ -25,6 +26,21 @@ export const Projets = () => {
                       <Nav.Link  eventKey="third">Tab 3</Nav.Link>
                     </Nav.Item>
                   </Nav>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="fisrt">
+                      <Row>
+                      {
+                          Pro.map((Pro , index) => {
+                            return (
+                              <p>{Pro.title}</p>
+                            )
+                          })
+                        }
+                      </Row>
+
+                    </Tab.Pane>
+                  </Tab.Content>
+                  </Tab.Container>
             </Col>
         </Row>
     </Container>
---
+++ src/util/pro.js
@@ -0,0 +1,32 @@
+const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
