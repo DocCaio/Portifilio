8fba055a6301dca8b4161cd897897f3aaffd0088
Caio <108128119+DocCaio@users.noreply.github.com>
2024-06-04 18:19:29 -0300

    up

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -8,9 +8,26 @@ export const ProjectsCards = () => {
    return (
 
  <section>
-   <h2>Projects</h2>
+   <h2>Projects</h2>   
    <div>
-     
+     {projects.map((projetct , id) => {
+      return (
+        <div key={id}>
+          <img src={projetct.imageSrc} alt= {`image of ${projetct.title}`}/>
+           <h3>{projetct.title}</h3>
+           <p>{projetct.description}</p>
+           <ul>
+            {projetct.skills.map((skills, id) => {
+              <li key={id}>{skills}</li>
+            })}
+           </ul>
+           <div>
+            <a href={projetct.demo}>Demo</a>
+            <a href={projetct.source}>Source</a>
+           </div>
+        </div>
+      )
+     })}
    </div>
  </section>
 
