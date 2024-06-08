af32952659d939b3b7083697371b6f4798a93259
Caio <108128119+DocCaio@users.noreply.github.com>
2024-06-08 10:27:46 -0300

    up

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 
 
 const   ContactForm = () => {
+ const [name ,setName] = useState('')
+ const [email ,setEmail] = useState('')
+ const [message ,setMessage] = useState('')
+
   return (
     <div className="contact-form-content">
-     <form>
+     <form className="form" onSubmit={() => {}}>
 
    <div className="name-container">
-     <input type="text" name="fistname" placeholder="First name"/>
-     <input type="text" name="lastname" placeholder="Last name"/>
+     <input type="text" name="fistname" placeholder="First name" onChange={(e) => setName(e.target.value)} value={name}/>
+     <input type="text" name="lastname" placeholder="Last name" onChange={(e) => setName(e.target.value)} value={name}/>
    </div>
 
-   <input type="text" name="email" placeholder="Email"/>
-   <textarea type="text" name="message" placeholder="Message" rows={3}/>
+   <input type="text" name="email" placeholder="Email"   onChange={(e) => setEmail(e.target.value)} value={email}/>
+   <textarea type="text"name="message" placeholder="Message" rows={3}  onChange={(e) => setMessage(e.target.value)}    value={message}   />
    <button>Send</button>
    </form>
    </div>
